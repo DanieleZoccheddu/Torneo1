b8735f2a0ea25fe3f9e6f2bb1f3ded326b8159a7
DanieleZoccheddu <68339833+DanieleZoccheddu@users.noreply.github.com>
2022-06-28 12:18:31 +0200

    prima classe

---
+++ src/Personaggio.java
@@ -0,0 +1,9 @@
+public class Personaggio {
+    private String nome;
+    private int puntiVita;
+    private int forzaFisica;
+    private float resistenzaFisica;
+    private float schivata;
+
+
+}
