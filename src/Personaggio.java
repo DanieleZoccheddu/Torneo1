8a5b60d773f1af7ac7cd2876d7e2f273404c325e
DanieleZoccheddu <68339833+DanieleZoccheddu@users.noreply.github.com>
2022-06-28 12:22:24 +0200

    prima classe.

--- src/Personaggio.java
+++ src/Personaggio.java
@@ -5,5 +5,12 @@ public class Personaggio {
     private float resistenzaFisica;
     private float schivata;
 
+    public Personaggio(String nome, int puntiVita, int forzaFisica, float resistenzaFisica, float schivata){
+        this.nome = nome;
+        this.puntiVita = puntiVita;
+        this.forzaFisica = forzaFisica;
+        this.resistenzaFisica = resistenzaFisica;
+        this.schivata = schivata;
+    }
 
 }
